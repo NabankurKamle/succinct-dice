6c52d2f4cabdda3a1c82bd829b9fb677144ba341
Nabankur Kamle <nabankurkamle@gmail.com>
2025-03-03 22:17:39 +0530

    font solved

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Poppins({
+const poppins = Poppins({
+  subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={geistSans.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false }; // Fix for Tailwind oxide
+    return config;
+  },
 };
 
 export default nextConfig;
